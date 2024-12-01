65cc97568efb269908aa63148c69b2f88b8e2add
victoriarahme <vic.rahme@gmail.com>
2024-12-01 11:32:30 -0500

    fixing js issues

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    // When "Show More" is clicked
+    $(".showMore a").on("click", function(e) {
+        e.preventDefault(); // stops link redirecting
+
+        var $this = $(this);
+        var $content = $this.closest(".project").find(".showLess");
+
+        var linkText = $this.text().toUpperCase();
+
+//Togglin here//
+if (linkText === "SHOW MORE") 
+{ linkText = "Show less"; $content.slideDown(); //to open up my project details 
+} else { linkText = "Show more"; $content.slideUp(); //closes project info/details
+    }
+$this.text(linkText); });});
+//Step 3: validation
